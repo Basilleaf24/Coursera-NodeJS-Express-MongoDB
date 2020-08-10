6584a4e718ca032b722ac1250b670f06387cd5f2
Prathibha R <prathi2kgrs@gmail.com>
2020-08-10 17:01:27 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -20,6 +20,33 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');//Buffer allows split option, space split is for separating Basic from string, : is for separating password and username
+  var user = auth[0];//username
+  var pass = auth[1];//password
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized and goes to the next set of statements for execution
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
