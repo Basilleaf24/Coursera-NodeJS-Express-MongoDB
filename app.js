435fccef64eb93420a17b6b6a4b5ae12bf9f67cf
Prathibha R <prathi2kgrs@gmail.com>
2020-08-11 17:25:12 +0530

    Expess Sessions Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');//middleware
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,10 +21,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth (req, res, next) {
-  if (!req.signedCookies.user) {//if cookie is not setup
+  console.log(req.session);
+
+  if (!req.session.user) {//if session is not setup
   var authHeader = req.headers.authorization;
   if (!authHeader) {//if user is not authorized
       var err = new Error('You are not authenticated!');
@@ -37,7 +47,7 @@ function auth (req, res, next) {
   var user = auth[0];//username
   var pass = auth[1];//password
   if (user == 'admin' && pass == 'password') {
-      res.cookie('user','admin',{signed: true});//setup cookie if user is authorized
+      res.session.user = 'admin';//sets up session if user is authenticated
       next(); //goes to the next set of statements for execution
   } else {
       var err = new Error('You are not authenticated!');
@@ -46,8 +56,9 @@ function auth (req, res, next) {
       next(err);
   }
 }
-else {//if cookies are already created
-  if (req.signedCookies.user === 'admin') {
+else {//if session is already created
+  if (req.session.user === 'admin') {
+      console.log('req.session: ',req.session);
       next();//executes next set of statements
   }
   else {
